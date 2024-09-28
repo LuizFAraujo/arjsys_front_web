f30922245942267b965122df8fe719c77e7af506
LuizFAraujo <luizfaraujo.eng@gmail.com>
2024-09-28 00:48:45 -0300

    Config Inicial

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white border border-gray-300 rounded-lg p-8 max-w-md shadow-md">
+        <h1 className="text-4xl font-semibold text-center text-gray-800">ArjSys</h1>
+      </div>
+    </div>
+  );
+}
