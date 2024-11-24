52720f5a9925e267ca12109bb7b10baecb939d90
LuizFAraujo <luizfaraujo.eng@gmail.com>
2024-11-24 15:38:28 -0300

    FEAT: Configuração da Navbar
    Adição:
    - Botão de hambúrguer na Navbar com ícone do Font Awesome
    - Botão de configurações na Navbar com ícone do Font Awesome
    - Estilo dos botões com cantos arredondados e efeito de hover
    - Logo do sistema ao centro

--- src/layouts/_partials/Navbar.tsx
+++ src/layouts/_partials/Navbar.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faCog } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar: React.FC = () => {
     return (
-        <div className="flex items-center justify-center p-4 h-full bg-custom-blue">
-            <h1 className="text-white text-lg font-semibold">NAVBAR</h1>
+        <div className="flex items-center justify-between p-4 h-full bg-custom-blue-1">
+            <IconButton icon={<FontAwesomeIcon icon={faBars} />} />
+            <h1 className="text-white text-lg font-bold">ArjSys</h1>
+            <IconButton icon={<FontAwesomeIcon icon={faCog} />} />
         </div>
     );
 };
 
+const IconButton: React.FC<{ icon: React.ReactNode }> = ({ icon }) => {
+    return (
+        <button className="w-9 h-9 flex items-center justify-center bg-custom-blue-1 rounded-lg hover:bg-custom-blue-2 text-white">
+            {icon}
+        </button>
+    );
+};
+
 export default Navbar;
-
-
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'custom-blue': 'rgb(52, 108, 176)',
+        'custom-blue-1': '#346CB0', // rgb(52, 108, 176)
+        'custom-blue-2': '#4D7EBA', // rgb(77, 126, 186)
       },
     },
   },
