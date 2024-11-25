96375053844cbe0a109085d5cd15798ba8c1ef10
LuizFAraujo <luizfaraujo.eng@gmail.com>
2024-11-25 01:34:59 -0300

    REFACTOR: Atualização dos ícones de setas na Sidebar
    - Substituição dos ícones de setas:
      - faChevronRight e faChevronDown substituídos por faCaretRight e faCaretDown
    - Ajuste do tamanho dos ícones e do texto para text-sm (14px)

--- src/layouts/_partials/Sidebar.tsx
+++ src/layouts/_partials/Sidebar.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faChevronDown, faCompressAlt } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom'; // Importe o Link do react-router-dom
+import {
+    faChevronRight, faChevronDown, faCompressAlt, faHome, faTachometerAlt, faProjectDiagram, faCogs,
+    faUsers, faShoppingCart, faEdit, faPlus, faSearch, faIndustry, faUserPlus, faDollarSign, faHeart,
+    faCaretRight, faCaretDown
+} from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Sidebar: React.FC = () => {
     const [openSubMenus, setOpenSubMenus] = useState<string[]>([]);
@@ -19,64 +23,64 @@ const Sidebar: React.FC = () => {
     };
 
     return (
-        <div className="flex flex-col flex-1 p-4 h-full overflow-y-auto bg-gray-100">
+        <div className="flex flex-col flex-1 p-2 h-full overflow-y-auto bg-gray-100">
             <div className="flex justify-between items-center mb-2">
-                <h1 className="text-lg font-semibold">Setores</h1>
+                <h1 className="text-base font-semibold">Setores</h1>
                 <button
                     className="text-gray-600 hover:text-gray-800"
                     onClick={closeAllSubMenus}
                     title="Fechar todos os submenus"
                 >
-                    <FontAwesomeIcon icon={faCompressAlt} />
+                    <FontAwesomeIcon icon={faCompressAlt} className="text-sm" />
                 </button>
             </div>
             <hr className="mx-0 border-t border-gray-300 mb-2" />
 
             {/* SubMenu Favoritos */}
-            <SubMenu title="Favoritos" isOpen={openSubMenus.includes('Favoritos')} toggleSubMenu={toggleSubMenu}>
-                <MenuItem title="Função Adicionada" to="/EmConstrucao" />
-                <MenuItem title="Função Adicionada" to="/EmConstrucao" />
-                <MenuItem title="Função Adicionada" to="/EmConstrucao" />
+            <SubMenu title="Favoritos" isOpen={openSubMenus.includes('Favoritos')} toggleSubMenu={toggleSubMenu} icon={faHeart}>
+                <MenuItem title="Função Adicionada" to="/EmConstrucao" icon={faPlus} />
+                <MenuItem title="Função Adicionada" to="/EmConstrucao" icon={faPlus} />
+                <MenuItem title="Função Adicionada" to="/EmConstrucao" icon={faPlus} />
             </SubMenu>
 
             <hr className="mx-0 border-t border-gray-300" />
 
             {/* Menu Items */}
-            <MenuItem title="Home" to="/" />
-            <MenuItem title="Dashboard" to="/Dashboard" />
+            <MenuItem title="Home" to="/" icon={faHome} />
+            <MenuItem title="Dashboard" to="/Dashboard" icon={faTachometerAlt} />
 
             {/* SubMenu Engenharia */}
-            <SubMenu title="Engenharia" isOpen={openSubMenus.includes('Engenharia')} toggleSubMenu={toggleSubMenu}>
-                <SubMenu title="Projetos" isOpen={openSubMenus.includes('Projetos')} toggleSubMenu={toggleSubMenu}>
-                    <SubMenu title="Cadastro" isOpen={openSubMenus.includes('Cadastro')} toggleSubMenu={toggleSubMenu}>
-                        <MenuItem title="Novo Item" to="/Cadastro_NovoItem" />
-                        <MenuItem title="Alimentar Estrutura" to="/Cadastro_AlimentarEstrutura" />
+            <SubMenu title="Engenharia" isOpen={openSubMenus.includes('Engenharia')} toggleSubMenu={toggleSubMenu} icon={faCogs}>
+                <SubMenu title="Projetos" isOpen={openSubMenus.includes('Projetos')} toggleSubMenu={toggleSubMenu} icon={faProjectDiagram}>
+                    <SubMenu title="Cadastro" isOpen={openSubMenus.includes('Cadastro')} toggleSubMenu={toggleSubMenu} icon={faEdit}>
+                        <MenuItem title="Novo Item" to="/Cadastro_NovoItem" icon={faPlus} />
+                        <MenuItem title="Alimentar Estrutura" to="/Cadastro_AlimentarEstrutura" icon={faPlus} />
                     </SubMenu>
-                    <SubMenu title="Consulta" isOpen={openSubMenus.includes('Consulta')} toggleSubMenu={toggleSubMenu}>
-                        <MenuItem title="Estrutura" to="/Consulta_Estrutura" />
+                    <SubMenu title="Consulta" isOpen={openSubMenus.includes('Consulta')} toggleSubMenu={toggleSubMenu} icon={faSearch}>
+                        <MenuItem title="Estrutura" to="/Consulta_Estrutura" icon={faSearch} />
                     </SubMenu>
                 </SubMenu>
-                <SubMenu title="Produção" isOpen={openSubMenus.includes('Produção')} toggleSubMenu={toggleSubMenu}>
-                    <SubMenu title="Cadastro" isOpen={openSubMenus.includes('Cadastro')} toggleSubMenu={toggleSubMenu}>
-                        <MenuItem title="Nova Ordem de Produção" to="/Cadastro_NovaOrdemProducao" />
+                <SubMenu title="Produção" isOpen={openSubMenus.includes('Produção')} toggleSubMenu={toggleSubMenu} icon={faIndustry}>
+                    <SubMenu title="Cadastro" isOpen={openSubMenus.includes('Cadastro')} toggleSubMenu={toggleSubMenu} icon={faEdit}>
+                        <MenuItem title="Nova Ordem de Produção" to="/Cadastro_NovaOrdemProducao" icon={faPlus} />
                     </SubMenu>
-                    <SubMenu title="Consulta" isOpen={openSubMenus.includes('Consulta')} toggleSubMenu={toggleSubMenu}>
-                        <MenuItem title="Ordens de Produção" to="/Consulta_OrdensDeProducao" />
-                        <MenuItem title="Vendas Efetuadas" to="/Consulta_VendasEfetuadas" />
+                    <SubMenu title="Consulta" isOpen={openSubMenus.includes('Consulta')} toggleSubMenu={toggleSubMenu} icon={faSearch}>
+                        <MenuItem title="Ordens de Produção" to="/Consulta_OrdensDeProducao" icon={faSearch} />
+                        <MenuItem title="Vendas Efetuadas" to="/Consulta_VendasEfetuadas" icon={faSearch} />
                     </SubMenu>
                 </SubMenu>
             </SubMenu>
 
             {/* SubMenu Comercial */}
-            <SubMenu title="Comercial" isOpen={openSubMenus.includes('Comercial')} toggleSubMenu={toggleSubMenu}>
-                <SubMenu title="Vendas" isOpen={openSubMenus.includes('Vendas')} toggleSubMenu={toggleSubMenu}>
-                    <SubMenu title="Cadastro" isOpen={openSubMenus.includes('Cadastro')} toggleSubMenu={toggleSubMenu}>
-                        <MenuItem title="Novo Cliente" to="/Cadastro_NovoCliente" />
-                        <MenuItem title="Nova Venda" to="/Cadastro_NovaVenda" />
+            <SubMenu title="Comercial" isOpen={openSubMenus.includes('Comercial')} toggleSubMenu={toggleSubMenu} icon={faShoppingCart}>
+                <SubMenu title="Vendas" isOpen={openSubMenus.includes('Vendas')} toggleSubMenu={toggleSubMenu} icon={faDollarSign}>
+                    <SubMenu title="Cadastro" isOpen={openSubMenus.includes('Cadastro')} toggleSubMenu={toggleSubMenu} icon={faEdit}>
+                        <MenuItem title="Novo Cliente" to="/Cadastro_NovoCliente" icon={faUserPlus} />
+                        <MenuItem title="Nova Venda" to="/Cadastro_NovaVenda" icon={faPlus} />
                     </SubMenu>
-                    <SubMenu title="Consulta" isOpen={openSubMenus.includes('Consulta')} toggleSubMenu={toggleSubMenu}>
-                        <MenuItem title="Clientes" to="/Consulta_Clientes" />
-                        <MenuItem title="Vendas Efetuadas" to="/Consulta_VendasEfetuadas" />
+                    <SubMenu title="Consulta" isOpen={openSubMenus.includes('Consulta')} toggleSubMenu={toggleSubMenu} icon={faSearch}>
+                        <MenuItem title="Clientes" to="/Consulta_Clientes" icon={faUsers} />
+                        <MenuItem title="Vendas Efetuadas" to="/Consulta_VendasEfetuadas" icon={faDollarSign} />
                     </SubMenu>
                 </SubMenu>
             </SubMenu>
@@ -86,23 +90,27 @@ const Sidebar: React.FC = () => {
 
 export default Sidebar;
 
-const MenuItem: React.FC<{ title: string; to?: string }> = ({ title, to }) => {
+const MenuItem: React.FC<{ title: string; to?: string; icon: any }> = ({ title, to, icon }) => {
     return (
-        <Link to={to || "#"} className="block px-4 py-2 text-gray-700 hover:bg-gray-200">
+        <Link to={to || "#"} className="flex items-center px-2 py-1 text-gray-700 text-sm hover:bg-gray-200">
+            <FontAwesomeIcon icon={icon} className="text-sm mr-2" />
             {title}
         </Link>
     );
 };
 
-const SubMenu: React.FC<{ title: string; isOpen: boolean; toggleSubMenu: (title: string) => void; children: React.ReactNode }> = ({ title, isOpen, toggleSubMenu, children }) => {
+const SubMenu: React.FC<{ title: string; isOpen: boolean; toggleSubMenu: (title: string) => void; children: React.ReactNode; icon: any }> = ({ title, isOpen, toggleSubMenu, children, icon }) => {
     return (
-        <div className="my-2">
+        <div className="my-1">
             <button
-                className="flex items-center justify-between w-full px-4 py-2 text-left text-gray-700 hover:bg-gray-200"
+                className="flex items-center justify-between w-full px-2 py-2 text-left text-gray-700 text-sm hover:bg-gray-200"
                 onClick={() => toggleSubMenu(title)}
             >
-                <span>{title}</span>
-                <FontAwesomeIcon icon={isOpen ? faChevronDown : faChevronRight} />
+                <div className="flex items-center">
+                    <FontAwesomeIcon icon={icon} className="text-sm mr-2" />
+                    <span>{title}</span>
+                </div>
+                <FontAwesomeIcon icon={isOpen ? faCaretDown : faCaretRight} className="text-sm" />
             </button>
             {isOpen && <div className="ml-4">{children}</div>}
         </div>
